8e51025a7ce42d84118cc8015f5fb820acb7429e
PushEax <pusheax.vk@gmail.com>
2025-05-08 00:01:22 +0300

    api help

---
+++ src/services/api.js
@@ -0,0 +1,180 @@
+const signupUser = {
+  url: '/users/signup',
+  method: 'POST',
+  requestBody: (name, email, password) => {
+    return {
+      name, //: 'Adrian Cross',
+      email, //: 'across@mail.com',
+      password, //: 'examplepwd12345',
+    };
+  },
+  responses: {
+    [201]: 'User created.',
+    [400]: 'User creation error.',
+    [500]: 'Server error.',
+  },
+};
+
+const loginUser = {
+  url: '/users/login',
+  method: 'POST',
+  requestBody: (email, password) => {
+    return {
+      email, //: 'string',
+      password, //: 'string',
+    };
+  },
+  responses: {
+    [200]: 'User is logged in.',
+    [400]: 'Login error.',
+  },
+};
+
+const logoutUser = {
+  url: '/users/logout',
+  method: 'POST',
+  headers: (token) => {
+    return {
+      Authorization: token,
+    };
+  },
+  responses: {
+    [200]: 'The user is logged out.',
+    [401]: 'Missing header with authorization token.',
+    [500]: 'Server error.',
+  },
+};
+
+const currentUser = {
+  url: '/users/current',
+  method: 'GET',
+  headers: (token) => {
+    return {
+      Authorization: token,
+    };
+  },
+  responses: {
+    [200]: 'Information found.',
+    [401]: 'Missing header with authorization token.',
+  },
+};
+
+const allContacts = {
+  url: '/contacts',
+  method: 'GET',
+  headers: (token) => {
+    return {
+      Authorization: token,
+    };
+  },
+  responses: {
+    [200]: 'Contacts found.',
+    [401]: 'Missing header with authorization token.',
+    [404]: 'There is no such user collection.',
+    [500]: 'Server error.',
+  },
+};
+
+const newContact = {
+  url: '/contacts',
+  method: 'POST',
+  headers: (token) => {
+    return {
+      Authorization: token,
+    };
+  },
+  requestBody: (name, number) => {
+    return {
+      name, //: 'Jacob Mercer',
+      number, //: '761-23-96',
+    };
+  },
+  responses: {
+    [201]: 'The contact was successfully created.',
+    [400]: 'Error creating contact.',
+    [401]: 'Missing header with authorization token.',
+  },
+};
+
+const deleteContact = {
+  url: (contactId) => `/contacts/${contactId}`,
+  method: 'DELETE',
+  headers: (token) => {
+    return {
+      Authorization: token,
+    };
+  },
+  responses: {
+    [200]: 'The contact was successfully deleted.',
+    [401]: 'Missing header with authorization token.',
+    [404]: 'There is no such user collection.',
+    [500]: 'Server error.',
+  },
+};
+
+const patchContact = {
+  url: (contactId) => `/contacts/${contactId}`,
+  method: 'PATCH',
+  headers: (token) => {
+    return {
+      Authorization: token,
+    };
+  },
+  requestBody: (name, number) => {
+    return {
+      name, //: 'Jacob Mercer',
+      number, //: '761-23-96',
+    };
+  },
+  responses: {
+    [200]: 'The contact was successfully updated.',
+    [400]: 'Contact update failed.',
+    [401]: 'Missing header with authorization token.',
+  },
+};
+
+const contactsApi = {
+  signup: {
+    signup: signupUser,
+    login: loginUser,
+    logout: logoutUser,
+    current: currentUser,
+  },
+  contact: {
+    all: allContacts,
+    new: newContact,
+    delete: deleteContact,
+    patch: patchContact,
+  },
+};
+
+export default contactsApi;
+
+// User{
+// id	string
+// Backend-generated unique identifier.
+
+// name*	string
+// Username.
+
+// email*	string
+// E-mail address.
+
+// password*	string
+// Password.
+
+// }
+// example: OrderedMap { "name": "Adrian Cross", "email": "across@mail.com", "password": "examplepwd12345" }
+
+// Contact{
+// id	string
+// Backend-generated unique identifier.
+
+// name*	string
+// Contact name.
+
+// number*	string
+// Phone number of the contact.
+
+// }
+// example: OrderedMap { "name": "Jacob Mercer", "number": "761-23-96" }
