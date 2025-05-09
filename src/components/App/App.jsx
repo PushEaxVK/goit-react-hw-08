67eaa074a4f57621338c4ae59d2493075c971e54
PushEax <pusheax.vk@gmail.com>
2025-05-09 15:46:04 +0300

    move App to components

---
+++ src/components/App/App.jsx
@@ -0,0 +1,31 @@
+import { useDispatch, useSelector } from 'react-redux';
+import css from './App.module.css';
+import ContactForm from '../ContactForm/ContactForm';
+import ContactList from '../ContactList/ContactList';
+import SearchBox from '../SearchBox/SearchBox';
+import { useEffect } from 'react';
+import { fetchContacts } from '../../redux/contacts/operations';
+import { selectError, selectLoading } from '../../redux/contacts/selectors';
+
+function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div className={css.app}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      {isLoading && !error && <b>Loading contacts...</b>}
+      {error && <b>{error}</b>}
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
