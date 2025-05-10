2a8e5fa01686333feb0de023db137e467fc216b7
PushEax <pusheax.vk@gmail.com>
2025-05-10 21:03:16 +0300

    login

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,46 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import s from './LoginForm.module.css';
+import { useDispatch } from 'react-redux';
+import { login } from '../../redux/auth/operations';
+
 const LoginForm = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+
+  const initialValues = { email: '', password: '' };
+
+  const handleSubmit = (values, options) => {
+    dispatch(login(values));
+    options.resetForm();
+  };
+
+  return (
+    <div className={s.wrapper}>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form className={s.form}>
+          <label htmlFor="email" className={s.label}>
+            Email
+          </label>
+          <Field
+            name="email"
+            type="email"
+            placeholder="Email"
+            id="email"
+            className={s.field}
+          />
+          <label htmlFor="password" className={s.label}>
+            Password
+          </label>
+          <Field
+            name="password"
+            type="password"
+            placeholder="Password"
+            id="password"
+            className={s.field}
+          />
+          <button type="submit">Login</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 export default LoginForm;
