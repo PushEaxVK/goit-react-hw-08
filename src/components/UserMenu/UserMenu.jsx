1703f4104ed28e0c32e9d574b80b6b77e9484170
PushEax <pusheax.vk@gmail.com>
2025-05-10 16:59:56 +0300

    add UserMenu

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,4 +1,21 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUser } from '../../redux/auth/selectors';
+import { logout } from '../../redux/auth/operations';
+import s from './UserMenu.module.css';
+
 const UserMenu = () => {
-  return <div>UserMenu</div>;
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  // user.name = 'Vasya'
+
+  return (
+    <div className={s.wrapper}>
+      <p>Welcome, {user.name}</p>
+      <button type="button" onClick={() => dispatch(logout())}>
+        Logout
+      </button>
+    </div>
+  );
 };
+
 export default UserMenu;
