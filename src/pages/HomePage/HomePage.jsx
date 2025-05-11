8380657cb59f0b5fda4494a77858f8191506260e
PushEax <pusheax.vk@gmail.com>
2025-05-11 17:19:17 +0300

    remove Container from HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,28 +6,24 @@ const HomePage = () => {
   const user = useSelector(selectUser);
 
   return (
-    <Container maxWidth="md" sx={{ py: 6 }}>
-      {/* <Typography variant="h4" align="center">
-        Home page
-      </Typography> */}
-      <Box textAlign="center">
-        <Typography variant="h3" gutterBottom>
-          📇 Welcome{user?.name ? `, ${user.name}` : ''}!
-        </Typography>
+    <Box textAlign="center">
+      <Typography variant="h3" gutterBottom>
+        📇 Welcome{user?.name ? `, ${user.name}` : ''}!
+      </Typography>
 
-        <Typography variant="h6" color="text.secondary" paragraph>
-          This is your personal contact book. You can:
-        </Typography>
+      <Typography variant="h6" color="text.secondary" paragraph>
+        This is your personal contact book. You can:
+      </Typography>
 
-        <Box textAlign="left" maxWidth="sm" mx="auto" mb={4}>
-          <ul style={{ fontSize: '1.1rem', lineHeight: 1.8 }}>
-            <li>Save important contacts with name and phone number</li>
-            <li>Search, filter, and edit your contacts</li>
-            <li>Secure your data with authentication</li>
-          </ul>
-        </Box>
+      <Box textAlign="left" maxWidth="sm" mx="auto" mb={4}>
+        <ul style={{ fontSize: '1.1rem', lineHeight: 1.8 }}>
+          <li>Save important contacts with name and phone number</li>
+          <li>Search, filter, and edit your contacts</li>
+          <li>Secure your data with authentication</li>
+        </ul>
       </Box>
-    </Container>
+    </Box>
   );
 };
+
 export default HomePage;
