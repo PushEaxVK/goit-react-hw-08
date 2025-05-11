a9b32c08e67eac3ff5922e073736936087dbf367
PushEax <pusheax.vk@gmail.com>
2025-05-11 17:18:56 +0300

    add Container to Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,13 @@
+import { Container } from '@mui/material';
 import AppBar from '../AppBar/AppBar';
 
 const Layout = ({ children }) => {
   return (
     <div>
       <AppBar />
-      {children}
+      <Container maxWidth="md" sx={{ py: 6 }}>
+        {children}
+      </Container>
     </div>
   );
 };
